d1603de85841d882709e93ac38230d93b2248bb8
Tomasz Polski <tomaszpolski21@gmail.com>
2024-11-04 23:02:57 +0100

    new file:   ObjCounter.hpp 	new file:   Statki.hpp 	new file:   Stocznia.hpp 	new file:   catch.hpp 	modified:   transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,23 @@
 #pragma once
 
 #include "Stocznia.hpp"
+#include "ObjCounter.hpp"
+#include "Statki.hpp"
 
-unsigned int transportujFlota(unsigned int towar)
-{
-    // Twoja implementacja tutaj
-    return 0;
+
+unsigned int transportujFlota(unsigned int towar) {
+    if (towar == 0) return 0; 
+
+    Stocznia stocznia; 
+    Stocznia::resetTotalCap(); 
+    CountThis<Zaglowiec>::reset();  
+
+    unsigned int transported = 0; 
+
+    while (transported < towar) {
+        Statek* statek = stocznia();  
+        transported += statek->transportuj(); 
+    }
+
+    return CountThis<Zaglowiec>::get(); 
 }
